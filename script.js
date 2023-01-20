c6025fbd592e20d1501c57f97443508a12133093
Subhajit Guchhait <74090703+imsubhajit98@users.noreply.github.com>
2023-01-20 19:37:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 function convert(){
-	let num=document.getElementById("roman");
-	convertToRoman(num);
+	// let num=document.getElementById("roman").value;
+
+	// let normal=[1000,900,500,400,100,90,50,40,10,9,5,4,1];
+ //        let roman=["M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"];
+ //        let res="";
+ //        for(let i=0;i<13;i++){
+ //            while(num>=normal[i]){
+ //                res=res+roman[i];
+ //                num=num-normal[i];
+ //            }
+ //        }
+	let roman=document.getElementById("letter");
+	roman.innerHTML="hello";
+	
+	// convertToRoman(num);
 }
 
 function convertToRoman(num) {
@@ -14,15 +27,14 @@ function convertToRoman(num) {
       6:['I', 1]
     };
 
-  //your code here
+//   //your code here
 
-}
+// }
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
 
 // console.log(convertToRoman(36));
 
 
-
-
 // do not edit below this line
 module.exports = convertToRoman
+}
