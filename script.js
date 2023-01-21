a5e93470f5a0e773cce39306fe9fb76d861ea1bb
Subhajit Guchhait <74090703+imsubhajit98@users.noreply.github.com>
2023-01-22 00:20:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,17 +1,9 @@
 function convert(){
 	let num=document.getElementById("roman").value;
 
-        let normal=[1000,900,500,400,100,90,50,40,10,9,5,4,1];
-        let roman=["M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"];
-        let res="";
-        for(let i=0;i<13;i++){
-            while(num>=normal[i]){
-                res=res+roman[i];
-                num=num-normal[i];
-            }
-        }
-	let romanRes=document.getElementById("letter");
-	romanRes.innerHTML=res;
+        
+	// let romanRes=document.getElementById("letter");
+	// romanRes.innerHTML=res;
 	
 	// convertToRoman(num);
 }
@@ -28,8 +20,17 @@ function convertToRoman(num) {
     };
 
 //   //your code here
-
-// }
+	let normal=[1000,900,500,400,100,90,50,40,10,9,5,4,1];
+        let roman=["M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"];
+        let res="";
+        for(let i=0;i<13;i++){
+            while(num>=normal[i]){
+                res=res+roman[i];
+                num=num-normal[i];
+            }
+        }
+return res;
+}
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
 
 // console.log(convertToRoman(36));
