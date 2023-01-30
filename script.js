a9eb28f2b99535fcbaa2ab3cc11ebd0297ec87c3
Subhajit Guchhait <74090703+imsubhajit98@users.noreply.github.com>
2023-01-30 10:48:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,21 +1,3 @@
-function convert(){
-	let num=document.getElementById("roman").value;
-
-        let normal=[1000,900,500,400,100,90,50,40,10,9,5,4,1];
-        let roman=["M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"];
-        let res="";
-        for(let i=0;i<13;i++){
-            while(num>=normal[i]){
-                res=res+roman[i];
-                num=num-normal[i];
-            }
-        }
-	let romanRes=document.getElementById("letter");
-	romanRes.innerHTML=res;
-	
-	// convertToRoman(num);
-}
-
 function convertToRoman(num) {
   	const obj = {
       0:['M',1000], 
@@ -26,24 +8,66 @@ function convertToRoman(num) {
       5:['V', 5], 
       6:['I', 1]
     };
-	let normal=[1000,900,500,400,100,90,50,40,10,9,5,4,1];
-        let roman=["M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"];
-        let res="";
-        for(let i=0;i<13;i++){
-            while(num>=normal[i]){
-                res=res+roman[i];
-                num=num-normal[i];
+
+ let result = "";
+   while(num){
+      if(num>=1000){
+         result += "M";
+         num -= 1000;
+      }else if(num>=500){
+         if(num>=900){
+            result += "CM";
+            num -= 900;
+         }else{
+            result += "D";
+            num -= 500;
+         }
+         }else if(num>=100){
+            if(num>=400){
+               result += "CD";
+               num -= 400;
+            }else{
+               result += "C";
+               num -= 100;
             }
-			return res;
-
-//   //your code here
-
+         }else if(num>=50){
+            if(num>=90){
+               result += "XC";
+               num -= 90;
+            }else{
+               result += "L";
+               num -= 50;
+            }
+         }else if(num>=10){
+            if(num>=40){
+               result += "XL";
+               num -= 40;
+            }else{
+               result += "X";
+               num -= 10;
+            }
+         }else if(num>=5){
+            if(num>=9){
+               result += "IX";
+               num -= 9;
+           }else{
+               result += "V";
+               num -= 5;
+            }
+         }else{
+            if(num>=4){
+               result += "IV";
+               num -= 4;
+         }else{
+            result += "I";
+            num -= 1;
+         }
+      }
+   }
+   return result;
 }
-// You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
 
 // console.log(convertToRoman(36));
 
-
 // do not edit below this line
 module.exports = convertToRoman
-// }
