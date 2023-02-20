2bb9fef9f3cc4e4f017319a16f17dcb451f8ecb8
Subhajit Guchhait <74090703+imsubhajit98@users.noreply.github.com>
2023-02-20 13:33:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -73,4 +73,37 @@ function convertToRoman(num) {
 // console.log(convertToRoman(36));
 
 // do not edit below this line
+
+
+
+
+/*
+function convertToRoman(num) {
+    const obj = {
+        M: 1000,
+        CM: 900,
+        D: 500,
+        CD: 400,
+        C: 100,
+        XC: 90,
+        L: 50,
+        XL: 40,
+        X: 10,
+        IX: 9,
+        V: 5,
+        IV: 4,
+        I: 1
+      };
+      let result = "";
+      for (let i in obj) {
+        while (num >= obj[i]) {
+          result += i;
+          num -= obj[i];
+        }
+      }
+      return result;
+ 
+}
+
+*/
 module.exports = convertToRoman
